d08fa944404a844b382057e34d7839a3f8e9ee42
Alex Abraham <alex.abraham@vimeo.com>
2020-03-10 14:51:32 -0400

    reverted stuff

--- torahbot/lib/yutorah.py
+++ torahbot/lib/yutorah.py
@@ -1,28 +1,32 @@
 import requests
 import json
 from configparser import SectionProxy
+import logging
+from bs4 import BeautifulSoup
+from random import choice
 
 
 class YUTorahClient:
 
     def __init__(self, config: SectionProxy):
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.INFO)
+        self.logger = logging.getLogger("YUTorahClient")
+
         self.url_template = config["SHIUR_LOOKUP_URL_TEMPLATE"]
         self.proxy_rotator_url = config["PROXY_ROTATOR_URL"]
         self.yu_request_timeout = int(config["YUZMANIM_REQUEST_TIMEOUT"])
-
-    def _proxy_generator(self):
-        data = json.loads(requests.get(self.proxy_rotator_url).text)
-        return {"https": data["proxy"]}
+        self.yu_request_tries = int(config["YUZMANIM_REQUEST_TRIES"])
 
     def get_shiurim_by_teacher(self, teacher_id, page=1):
         url = self.url_template.format(
             page,
             teacher_id
         )
+        self.logger.info("getting shiur data: {}".format(url))
         response = requests.get(
-            url=url,
-            proxies=self._proxy_generator(),
-            timeout=self.yu_request_timeout
+            url=url
         )
         data = json.loads(response.text)
+        self.logger.info("shiur data: {}".format(response.text))
         return data["response"]["docs"]

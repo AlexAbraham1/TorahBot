295314682b80029d1becb067ee28f5b8a52028e7
Alex Abraham <alex.abraham@vimeo.com>
2020-03-09 22:20:12 -0400

    added systemd service file for raspberry pi + berryconda

--- torahbot/lib/yutorah.py
+++ torahbot/lib/yutorah.py
@@ -7,11 +7,22 @@ class YUTorahClient:
 
     def __init__(self, config: SectionProxy):
         self.url_template = config["SHIUR_LOOKUP_URL_TEMPLATE"]
+        self.proxy_rotator_url = config["PROXY_ROTATOR_URL"]
+        self.yu_request_timeout = int(config["YUZMANIM_REQUEST_TIMEOUT"])
+
+    def _proxy_generator(self):
+        data = json.loads(requests.get(self.proxy_rotator_url).text)
+        return {"https": data["proxy"]}
 
     def get_shiurim_by_teacher(self, teacher_id, page=1):
         url = self.url_template.format(
             page,
             teacher_id
         )
-        data = json.loads(requests.get(url).text)
+        response = requests.get(
+            url=url,
+            proxies=self._proxy_generator(),
+            timeout=self.yu_request_timeout
+        )
+        data = json.loads(response.text)
         return data["response"]["docs"]
